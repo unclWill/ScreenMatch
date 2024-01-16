5e92b9492b0c065a2b2ed209130429dddc7d691f
William Silva <william.silva@viannasempre.com.br>
2024-01-16 09:42:12 -0300

    Adição de novos exercícios

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -5,18 +5,22 @@
  */
 
 public class Filme {
-    public String nome;
-    public int anoDeLancamento;
-    public boolean incluidoNoPlano;
-    public double somaDasAvaliacoes;
-    public int totalDeAvaliacoes;
-    public int duracaoEmMinutos;
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
 
     public void exibeFichaTecnica() {
         System.out.printf("""
                 Nome do filme: \t\t%s
                 Ano de lançamento: \t%d
                 """, nome, anoDeLancamento);
+    }
+
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
     }
 
     public void avalia(double nota) {
